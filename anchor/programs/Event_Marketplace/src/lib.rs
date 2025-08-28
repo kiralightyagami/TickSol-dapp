aaed391815c652987bbffdf23d14e10d75c54a91
kiralightyagami <crucisgamer98@gmail.com>
2025-08-28 18:43:04 +0530

    program done

--- anchor/programs/Event_Marketplace/src/lib.rs
+++ anchor/programs/Event_Marketplace/src/lib.rs
@@ -1,16 +1,27 @@
 use anchor_lang::prelude::*;
 
+
 declare_id!("");
 
+pub mod instructions;
+pub mod error;
+pub mod states;
+
+use instructions::*;
+
 #[program]
-pub mod Event_Marketplace {
+pub mod event_marketplace {
     use super::*;
 
-    pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("GM!");
-        Ok(())
+    pub fn initialize(ctx: Context<InitializeContext>, name: String, description: String, ticket_price: u64, available_tickets: u64, start_date: i64) -> Result<()> {
+        _initialize(ctx, name, description, ticket_price, available_tickets, start_date)
+    }
+    
+    pub fn buy(ctx: Context<BuyContext>) -> Result<()> {
+        _buy(ctx)
+    }
+
+    pub fn withdraw(ctx: Context<WithdrawContext>, amount: u64) -> Result<()> {
+        _withdraw(ctx, amount)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
